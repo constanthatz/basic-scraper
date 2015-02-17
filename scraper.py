9d5c59c7f54d7c4f46331197f50b3aab2da28754
Constantine Hatzis <constantine.hatzis@gmail.com>
2015-02-16 20:57:20 -0800

    add has two tds function. response from webpage contains no results.

--- scraper.py
+++ scraper.py
@@ -4,42 +4,47 @@ import sys
 import re
 
 
-DOMAIN = 'http://info.kingcounty.gov'
-PATH = '/health/ehs/foodsafety/inspections/Results.aspx'
-PARAMETERS = {'Output': 'W',
-              'Business_Name': '',
-              'Business_Address': '',
-              'Longitude': '',
-              'Latitude': '',
-              'City': '',
-              'Zip_Code': '98103',
-              'Inspection_Type': 'All',
-              'Inspection_Start': '',
-              'Inspection_End': '',
-              'Inspection_Closed_Business': 'A',
-              'Violation_Points': '',
-              'Violation_Red_Points': '',
-              'Violation_Descr': '',
-              'Fuzzy_Search': 'N',
-              'Sort': '',
-              }
+INSPECTION_DOMAIN = 'http://info.kingcounty.gov'
+INSPECTION_PATH = '/health/ehs/foodsafety/inspections/Results.aspx'
+INSPECTION_PARAMS = {'Output': 'W',
+                     'Business_Name': '',
+                     'Business_Address': '',
+                     'Longitude': '',
+                     'Latitude': '',
+                     'City': '',
+                     'Zip_Code': '98103',
+                     'Inspection_Type': 'All',
+                     'Inspection_Start': '',
+                     'Inspection_End': '',
+                     'Inspection_Closed_Business': 'A',
+                     'Violation_Points': '',
+                     'Violation_Red_Points': '',
+                     'Violation_Descr': '',
+                     'Fuzzy_Search': 'N',
+                     'Sort': '',
+                     }
 
 
 def get_inspection_page(**kwargs):
-    url = DOMAIN + PATH
-    parameters = PARAMETERS.copy()
+    url = INSPECTION_DOMAIN + INSPECTION_PATH
+    params = INSPECTION_PARAMS.copy()
     for key, val in kwargs.items():
-        if key in PARAMETERS:
-            parameters[key] = val
-    response = requests.get(url, params=parameters)
-    response.raise_for_status()
-    return response.encoding, response.content
+        if key in INSPECTION_PARAMS:
+            params[key] = val
+    resp = requests.get(url, params=params)
+    resp.raise_for_status()
+    with open('inspection_page.html', "w") as fo:
+        fo.write('%s\n%s' % (resp.encoding, resp.content))
+    return resp.content, resp.encoding
 
 
 def load_inspection_page(file):
     with open('inspection_page.html', "r") as fo:
-        html = fo.read()
-    return html
+        encoding = fo.readline()
+        html = ''
+        for line in fo:
+            html += line
+    return html, encoding
 
 
 def parse_source(html, encoding='utf-8'):
@@ -52,6 +57,12 @@ def extract_data_listings(html):
     return html.find_all('div', id=id_finder)
 
 
+def has_two_tds(elem):
+    is_tr = elem.name == 'tr'
+    td_children = elem.find_all('td', recursive=False)
+    has_two = len(td_children) == 2
+    return is_tr and has_two
+
 if __name__ == '__main__':
     kwargs = {
         'Inspection_Start': '2/1/2013',
@@ -60,10 +71,11 @@ if __name__ == '__main__':
     }
 
     if len(sys.argv) > 1 and sys.argv[1] == 'test':
-        # you will likely have something different here, depending on how
-        # you implemented the load_inspection_page function.
         html, encoding = load_inspection_page('inspection_page.html')
     else:
         html, encoding = get_inspection_page(**kwargs)
     doc = parse_source(html, encoding)
+    listings = extract_data_listings(doc)
+    print len(listings)
+    print listings[0].prettify()
     print doc.prettify(encoding=encoding)
