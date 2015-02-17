761c5e0dc84eb2550724668e891600dcff87b346
Constantine Hatzis <constantine.hatzis@gmail.com>
2015-02-16 18:47:46 -0800

    make strings strings.

--- scraper.py
+++ scraper.py
@@ -4,20 +4,20 @@ from __future__ import print_function
 
 DOMAIN = 'http://info.kingcounty.gov'
 PATH = '/health/ehs/foodsafety/inspections/Results.aspx'
-PARAMETERS = {Output: W,
-              Business_Name: &,
-              Business_Address: &,
-              Longitude: &,
-              Latitude: &,
-              City: &,
-              Zip_Code: &.
-              Inspection_Type: All&
-              Inspection_Start: &,
-              Inspection_End: &,
-              Inspection_Closed_Business: A&,
-              Violation_Points: &,
-              Violation_Red_Points: &,
-              Violation_Descr: &,
-              Fuzzy_Search: N,
-              Sort: &,
+PARAMETERS = {'Output': 'W',
+              'Business_Name': '',
+              'Business_Address': '',
+              'Longitude': '',
+              'Latitude': '',
+              'City': '',
+              'Zip_Code': '',
+              'Inspection_Type': 'All',
+              'Inspection_Start': '',
+              'Inspection_End': '',
+              'Inspection_Closed_Business': 'A',
+              'Violation_Points': '',
+              'Violation_Red_Points': '',
+              'Violation_Descr': '',
+              'Fuzzy_Search': 'N',
+              'Sort': '',
               }
