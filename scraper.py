80f0850874a1160d0bc001c7c47992d6956aec5e
Constantine Hatzis <constantine.hatzis@gmail.com>
2015-02-16 20:20:22 -0800

    add given code becuase of errors.

--- scraper.py
+++ scraper.py
@@ -36,15 +36,15 @@ def get_inspection_page(**kwargs):
     return response.encoding, response.content
 
 
-def read_html(file):
+def load_inspection_page(file):
     with open('inspection_page.html', "r") as fo:
         html = fo.read()
     return html
 
 
 def parse_source(html, encoding='utf-8'):
-    souped = BeautifulSoup(html, from_encoding=encoding)
-    return souped
+    parsed = BeautifulSoup(html, from_encoding=encoding)
+    return parsed
 
 
 def extract_data_listings(html):
@@ -53,17 +53,17 @@ def extract_data_listings(html):
 
 
 if __name__ == '__main__':
-    kwargs = {'Inspection_Start': '2/1/2013',
-              'Inspection_End': '2/1/2015',
-              'Zip_Code': '98109'}
+    kwargs = {
+        'Inspection_Start': '2/1/2013',
+        'Inspection_End': '2/1/2015',
+        'Zip_Code': '98109'
+    }
 
     if len(sys.argv) > 1 and sys.argv[1] == 'test':
-        html, encoding = read_html('inspection_page.html')
+        # you will likely have something different here, depending on how
+        # you implemented the load_inspection_page function.
+        html, encoding = load_inspection_page('inspection_page.html')
     else:
         html, encoding = get_inspection_page(**kwargs)
-    data = parse_source(html, encoding)
-
-    listings = extract_data_listings(data)
-    print len(listings)
-    print listings[0].prettify()
-    print data.prettify(encoding=encoding)
+    doc = parse_source(html, encoding)
+    print doc.prettify(encoding=encoding)
