926f4c6127af01bec8970f5cbe0583b05ebcda44
Constantine Hatzis <constantine.hatzis@gmail.com>
2015-02-16 18:45:31 -0800

    seperate query parameters into constants and a dictionary.

--- scraper.py
+++ scraper.py
@@ -1,18 +1,23 @@
-http://info.kingcounty.gov
-/health/ehs/foodsafety/inspections/Results.aspx
-?Output=W&
-Business_Name=morsel&
-Business_Address=&
-Longitude=&
-Latitude=&
-City=&
-Zip_Code=&
-Inspection_Type=All&
-Inspection_Start=&
-Inspection_End=&
-Inspection_Closed_Business=A&
-Violation_Points=&
-Violation_Red_Points=&
-Violation_Descr=&
-Fuzzy_Search=N&
-Sort=B
+#!/usr/bin/env python
+from __future__ import print_function
+
+
+DOMAIN = 'http://info.kingcounty.gov'
+PATH = '/health/ehs/foodsafety/inspections/Results.aspx'
+PARAMETERS = {Output: W,
+              Business_Name: &,
+              Business_Address: &,
+              Longitude: &,
+              Latitude: &,
+              City: &,
+              Zip_Code: &.
+              Inspection_Type: All&
+              Inspection_Start: &,
+              Inspection_End: &,
+              Inspection_Closed_Business: A&,
+              Violation_Points: &,
+              Violation_Red_Points: &,
+              Violation_Descr: &,
+              Fuzzy_Search: N,
+              Sort: &,
+              }
